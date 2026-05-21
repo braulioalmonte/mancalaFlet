9a3554c08003ef9f750216379cdfd27c43609411
Braulio Almonte <60824687+braulioalmonte@users.noreply.github.com>
2026-05-21 16:04:25 -0400

    first commit

---
+++ mancalaGUI.py
@@ -0,0 +1,138 @@
+import flet as ft
+import random
+import time
+
+#TODO 1: Finish core gameplay [DONE]
+#TODO 2: Add animations for played spaces
+#TODO 3: Finish play again mechanic
+#TODO 4: Add winning conditions and check
+#TODO 5: ??? -> Profit
+#TODO 6: Add option to change background
+
+#! There is a bug regarding the p1buttons, p2buttons lists and the turn logic, i'll fix it later
+#! Bug Fixed
+
+def main(page: ft.Page):
+    #variables
+    turn = random.randint(0,1)
+
+    #functions
+    def changeTurn():
+        nonlocal turn
+        disableButtons(turn)
+
+    def transferPoints(e):
+        nonlocal turn
+        position = e.control.data[0]
+        beads = e.control.data[1]
+        e.control.data[1] = 0
+        e.control.content.value = f"{0}"
+
+        for i in range(beads):
+            position = (position + 1) % 14 # <- Sneaky Idiot, was 13 and was giving me a looping logic mistake
+            if (position == 6 and turn == 0):
+                position = 7
+            elif (position == 13 and turn == 1):
+                position = 0
+
+            print(position)
+            orderList[position].data[1] += 1
+            orderList[position].content.value = f"{orderList[position].data[1]}"
+            # time.sleep(0.1) #this doesn't work, might use on_animation_end of buttons to simulate a delay
+        
+        print(f"---{turn+1}---")
+        #play again
+        if position == 13 and turn == 0:
+            turn = 0
+        elif position == 6 and turn == 1:
+            turn = 1
+        else:
+            turn = (turn+1) % 2
+
+        turnText.value = f"Turn: Player {turn+1}"
+        
+        #here goes the turn change function for the buttons (disabling and stuff)
+        changeTurn()
+
+    def disableButtons(turn):
+        if turn == 0:
+            #disable p2 buttons
+            for button in p2Buttons:
+                button.disabled = True
+            #enable p1 buttons
+            for button in p1Buttons:
+                button.disabled = False
+
+        elif turn == 1:
+            #disable p1 buttons
+            for button in p1Buttons:
+                button.disabled = True
+            #enable p2 buttons
+            for button in p2Buttons:
+                button.disabled = False
+
+    def checkWin():
+        pass
+
+    #debug
+
+    def printPosition(e):
+        print(e.control.data[0])
+
+    #page setup
+    page.title = "Mancala"
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    #control
+
+    p1Buttons = [ft.Button(width=100, 
+                           height=100, 
+                           content=ft.Text(value="4", 
+                                           size=20), 
+                            data = [i,4], 
+                            color=ft.Colors.BLUE,
+                            on_click=transferPoints)
+                            for i in range(12,6,-1)]
+    
+    p2Buttons = [ft.Button(width=100, 
+                           height=100, 
+                           content=ft.Text(value="4", 
+                                           size=20), 
+                            data = [i,4], 
+                            color=ft.Colors.RED,
+                            on_click=transferPoints) 
+                            for i in range(0,6)]
+
+    p1Row = ft.Row(controls=p1Buttons)
+    p2Row = ft.Row(controls=p2Buttons)
+
+    p1Space = ft.Button(bgcolor=ft.Colors.BLUE, 
+                        color=ft.Colors.BLACK, 
+                        content=ft.Text("0"), 
+                        data=[13,0], width=100, 
+                        height=200, 
+                        disabled=True)
+    
+    p2Space = ft.Button(bgcolor=ft.Colors.RED, 
+                        color=ft.Colors.BLACK, 
+                        content=ft.Text("0"), 
+                        data=[6,0],
+                        width=100, 
+                        height=200, 
+                        disabled=True)
+
+    p1Buttons = sorted(p1Buttons, key=lambda e: e.data[0])
+    orderList = p2Buttons + [p2Space] + p1Buttons + [p1Space]
+
+    for i in orderList:
+        print(i.data)
+
+    playerColumn = ft.Column(controls=[p1Row, p2Row])
+    finalRow = ft.Row(controls=[p1Space, playerColumn, p2Space], alignment=ft.MainAxisAlignment.CENTER)
+    turnText = ft.Text(value=f"Turn: Player {turn+1}")
+    disableButtons(turn)
+    page.add(turnText, finalRow)
+
+ft.run(main=main, assets_dir="assets")
